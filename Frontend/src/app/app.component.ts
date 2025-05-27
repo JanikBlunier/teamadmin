2919e7f4b167e5ab150714e42c2e7eb3a1460381
LAPTOP-JANIK\janik <jbl146596@stund.gibb.ch>
2025-05-27 13:29:28 +0200

    Adding new main components

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavComponent} from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
