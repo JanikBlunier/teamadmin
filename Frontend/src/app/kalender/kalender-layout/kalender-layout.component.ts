64874ddfcdbd171c2257db56938f74419088de4c
LAPTOP-JANIK\janik <jbl146596@stund.gibb.ch>
2025-05-28 15:08:11 +0200

    Adding the possibility to change between the weeks

--- Frontend/src/app/kalender/kalender-layout/kalender-layout.component.ts
+++ Frontend/src/app/kalender/kalender-layout/kalender-layout.component.ts
@@ -15,7 +15,37 @@ import {NgForOf, NgIf} from '@angular/common';
 })
 
 export class KalenderLayoutComponent {
+  currentWeekDates: string[] = [];
   daysOfWeek = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
+  referenceDate = new Date();
+
+  ngOnInit() {
+    this.generateCurrentWeek();
+  }
+
+  generateCurrentWeek() {
+    const firstDayOfWeek = new Date(this.referenceDate);
+    const dayOfWeek = (this.referenceDate.getDay() + 6) % 7; // Montag = 0
+    firstDayOfWeek.setDate(this.referenceDate.getDate() - dayOfWeek);
+
+    this.currentWeekDates = Array.from({ length: 7 }, (_, i) => {
+      const date = new Date(firstDayOfWeek);
+      date.setDate(firstDayOfWeek.getDate() + i);
+      const day = this.daysOfWeek[i];
+      const dateNum = date.getDate().toString().padStart(2, '0');
+      return `${day} ${dateNum}`;
+    });
+  }
+
+  previousWeek() {
+    this.referenceDate.setDate(this.referenceDate.getDate() - 7);
+    this.generateCurrentWeek();
+  }
+
+  nextWeek() {
+    this.referenceDate.setDate(this.referenceDate.getDate() + 7);
+    this.generateCurrentWeek();
+  }
 
   createEvent() {
     alert('Neues Event erstellen');
