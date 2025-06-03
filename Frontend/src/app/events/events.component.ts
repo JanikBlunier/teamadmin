3cdb99f87618d314e9241dd918e5935c88a111c9
jamil.burkhalter <jbu145960@stud.gibb.ch>
2025-06-03 11:40:12 +0200

    Buttons fürs Bearbeiten in Events hinzugefügt

--- Frontend/src/app/events/events.component.ts
+++ Frontend/src/app/events/events.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {DUMMY_EVENTS, EventData} from './Dummy-event';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import { Router } from '@angular/router';
+import {EditEventComponent} from '../kalender/edit-event/edit-event.component';
 
 @Component({
   selector: 'app-events',
   imports: [
-    NgForOf
+    NgForOf,
+    EditEventComponent,
+    NgIf
   ],
   templateUrl: './events.component.html',
   standalone: true,
@@ -16,5 +19,16 @@ import { Router } from '@angular/router';
 export class EventsComponent {
   events: EventData[] = DUMMY_EVENTS;
 
+  showEditEventForm = false;
+  selectedEvent?: EventData;
+
+  openEditEvent(event: EventData) {
+    this.selectedEvent = event;
+    this.showEditEventForm = true;
+  }
+
+  closeEditEvent() {
+    this.showEditEventForm = false;
+  }
 }
 
