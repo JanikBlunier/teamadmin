c9d161caa56fc9f65466c7ae83df2b0c451c9328
LAPTOP-JANIK\janik <jbl146596@stund.gibb.ch>
2025-06-03 10:39:21 +0200

    Adding edit-event component

---
+++ Frontend/src/app/kalender/edit-event/edit-event.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-event',
+  templateUrl: './edit-event.component.html',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  styleUrls: ['./edit-event.component.css']
+})
+export class EditEventComponent {
+
+}
